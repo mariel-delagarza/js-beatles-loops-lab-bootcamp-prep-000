d0dce2cb8c13eb0921f5183d2040e55bd146ded4
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-14 17:40:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var = array = [];
+  var array = [];
   for (var i = 0; i < musicians.legnth; i++) {
     array.push(musicians[i] + 'plays' + instruments[i]);
     }
