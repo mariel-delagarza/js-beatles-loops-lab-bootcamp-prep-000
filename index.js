02a77e48af1f738721edf2dc2bd0c004d99afadf
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-14 17:39:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var = {}
-  for ()
+  var = []
+  for (var i = 0; i < musicians.legnth; i++) {
+    array.push(musicians[i] + 'plays' + instruments[i]);
+    }
+  return array;
 }
