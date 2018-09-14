3c0d055a09ee6ec3a4d57065a27950fdc82b977c
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-14 17:58:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function theBeatlesPlay (musicians, instruments) {
 }
 function johnLennonFacts (facts) {
   var array = [];
+  var i = 0;
   while (i < facts.length) {
     array.push(facts[i] + "!!!");
     i++;
